59bd2f5e1883b4ab4b02d6618b7a1476ad3312ef
unknown <viniciusbeloti7@gmail.com>
2020-09-01 14:14:11 -0300

    feat: add items in navbar

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -3,7 +3,21 @@ import React from 'react';
 import './styles.css';
 
 const Nav: React.FC = () => {
-  return <div />;
+  return (
+      <div className="nav">
+          <h2>Tendências</h2>
+          <h2>Mais Votados</h2>
+          <h2>Ação</h2>
+          <h2>Comédia</h2>
+          <h2>Teror</h2>
+          <h2>Romance</h2>
+          <h2>Mistério</h2>
+          <h2>Sci-fi</h2>
+          <h2>Ocidental</h2>
+          <h2>Animação</h2>
+          <h2>Filme</h2>
+      </div>
+  );
 }
 
 export default Nav;
